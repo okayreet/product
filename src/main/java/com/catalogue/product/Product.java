c7cab444689cc9362e33ab3e3315d40086583635
darkerror <darkerror@hotmail.co.uk>
2023-03-25 17:32:49 +0000

    rename table

--- src/main/java/com/catalogue/product/Product.java
+++ src/main/java/com/catalogue/product/Product.java
@@ -5,10 +5,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "orders")
 public class Product {
     @Id
     @SequenceGenerator(name = "product_id_sequence", sequenceName = "product_id_sequence")
